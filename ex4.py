9781b84fd6f15fb730ec42995c3e2ac0706e22d4
James Lawlor <jalawlor@tcd.ie>
2016-02-08 11:36:00 +0000

    exercise 4 added

---
+++ ex4.py
@@ -0,0 +1,24 @@
+"""
+=================================================
+Project Euler Exercise 4
+=================================================
+A palindromic number reads the same both ways. The largest palindrome: made from
+the product of two 2-digit numbers is 9009 = 91 x 99.
+Find the largest palindrome made from the product of two 3-digit numbers.
+"""
+# Author: James Lawlor <jalawlor@tcd.ie>
+
+def palindromeCheck(x):
+    """
+    Checks to see if integer x is a palindrome
+    """
+    return str(x)==str(x)[::-1]
+
+
+largest = 0.0
+# run through lists of all 3 digit numbers
+for a,b in zip(range(999,100,-1),range(999,100,-1)):
+    if palindromeCheck(a*b) == True and a*b > largest:
+	    largest = a*b
+
+print largest			
