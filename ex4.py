f5789199b812d257af0e80dc1b9e113b5456c643
James Lawlor <jalawlor@tcd.ie>
2016-06-20 16:29:40 +0100

    Update ex4.py

--- ex4.py
+++ ex4.py
@@ -7,18 +7,11 @@ the product of two 2-digit numbers is 9009 = 91 x 99.
 Find the largest palindrome made from the product of two 3-digit numbers.
 """
 # Author: James Lawlor <jalawlor@tcd.ie>
-
-def palindromeCheck(x):
-    """
-    Checks to see if integer x is a palindrome
-    """
-    return str(x)==str(x)[::-1]
-
-
+def palindromeCheck(x): # Checks to see if integer x is a palindrome
+  return str(x)==str(x)[::-1]
 largest = 0.0
 # run through lists of all 3 digit numbers
 for a,b in zip(range(999,100,-1),range(999,100,-1)):
-    if palindromeCheck(a*b) == True and a*b > largest:
-	    largest = a*b
-
+  if palindromeCheck(a*b) == True and a*b > largest:
+    largest = a*b
 print largest			
