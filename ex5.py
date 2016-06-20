334e1f94e57435b2d95bd0628aa3b45f6279040d
James Lawlor <jalawlor@tcd.ie>
2016-06-20 16:27:06 +0100

    ex5 added

---
+++ ex5.py
@@ -0,0 +1,24 @@
+"""
+=================================================
+Project Euler Exercise 5
+=================================================
+2520 is the smallest number that can be divided by each of the numbers from 1 to
+10 without any remainder. What is the smallest positive number that is evenly
+ divisible by all of the numbers from 1 to 20?
+"""
+# Author: James Lawlor <jalawlor@tcd.ie>
+
+def divisible(x,y = 20):
+
+    for i in range(y, 0,-1):
+
+        if x % i != 0: return False
+
+	return True
+
+j = 3
+
+while divisible(j,10) != True:
+	j+=1
+
+print j
